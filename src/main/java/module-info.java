a3929ea72961e236f55d1eb9474e736fd0844dfe
Alex Morehead <alex.morehead@gmail.com>
2020-03-29 16:26:05 -0500

    Resolve Maven Compilation Conflicts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.github.amorehead {
     requires javafx.controls;
     requires javafx.fxml;
     requires poi.ooxml;
+    requires java.desktop;
 
     opens io.github.amorehead to javafx.fxml;
     exports io.github.amorehead;
