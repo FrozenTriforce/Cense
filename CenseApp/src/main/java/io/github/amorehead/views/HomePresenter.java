f99f9325103d8af19ba548bca71a8346884699c3
Alex Morehead <alex.morehead@gmail.com>
2020-05-03 15:07:59 -0500

    Create Functionality for Opening README Hyperlink upon Clicking Button on Home View

--- CenseApp/src/main/java/io/github/amorehead/views/HomePresenter.java
+++ CenseApp/src/main/java/io/github/amorehead/views/HomePresenter.java
@@ -5,32 +5,36 @@ import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class HomePresenter {
 
     @FXML
     private View main;
 
-    @FXML
-    private Label label;
-
     public void initialize() {
         main.showingProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue) {
                 AppBar appBar = MobileApplication.getInstance().getAppBar();
-                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> 
+                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e ->
                         MobileApplication.getInstance().getDrawer().open()));
                 appBar.setTitleText("Main");
-                appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> 
+                appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e ->
                         System.out.println("Search")));
             }
         });
     }
-    
+
     @FXML
     void buttonClick() {
-        label.setText("Hello JavaFX Universe!");
+        try {
+            Desktop.getDesktop().browse(new URI("https://www.github.com/amorehead/cense"));
+        } catch (IOException | URISyntaxException e1) {
+            e1.printStackTrace();
+        }
     }
-    
 }
